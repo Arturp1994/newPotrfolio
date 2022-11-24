3b703c11edc2af8a34b32b5143790be47a67cf8d
Arthur <arturiopr1994@gmail.com>
2022-11-24 14:03:30 -0800

    New components2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import './App.scss'
+import './App.css'
+import {Header} from "./Component/Header/Header";
 
 function App() {
-    return <></>;
+    return (
+        <Header/>
+    );
 }
 
 export default App;
--- src/Component/Experience/Experience.tsx
+++ src/Component/Experience/Experience.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import s from './Experience.module.scss'
+
 
 export const Experience = ()=>{
     return(
